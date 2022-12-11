6daf3c8980f802fe144db1720bd8a124af10b77a
Denisse <landau@192.168.1.8>
2022-12-11 20:45:49 -0300

    get method

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,23 +9,20 @@ from models.state import State
 from models.city import City
 
 
-@app_views.route('/states/<s_id>/cities', methods=['GET'], strict_slashes=False)
-def get_all_cities(s_id):
-    """gets all city"""
-    themcity = []
-    state = storage.get(State, s_id)
-
-    if state is None:
+@app_views.route("/states/<state_id>/cities", methods=['GET'], strict_slashes=False)
+def cities_index(state_id):
+    if storage.get(State, state_id) is None:
         abort(404)
-    for city in state.cities:
-        themcity.append(city.to_dict())
-    return jsonify(themcity)
+
+    cities = storage.get(State, state_id).cities
+    cities_dict = []
+    for city in cities:
+        cities_dict.append(city.to_dict())
+    return jsonify(cities_dict)
 
 
-@app_views.route("/cities/<c_id>", methods=['GET'], strict_slashes=False)
-def get_city_id(c_id):
-    """gets city by id"""
-    city = storage.get(City, c_id)
-    if city is None:
+@app_views.route("/cities/<city_id>", methods=['GET'], strict_slashes=False)
+def cities_get(city_id):
+    if storage.get(City, city_id) is None:
         abort(404)
-    return city.to_dict()
+    return jsonify(storage.get(City, city_id).to_dict())
