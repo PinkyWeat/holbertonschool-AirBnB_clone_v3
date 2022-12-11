ec9d1ee8be74d8f8b5e2d63d43716da524e0d0df
Denisse <landau@192.168.1.8>
2022-12-11 18:29:57 -0300

    pep8

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -12,12 +12,12 @@ from models.state import State
 @app_views.route('/states/<s_id>', methods=['GET'], strict_slashes=False)
 def get_state(s_id=None):
     """if no state id is given, retrieves all"""
-    if s_id is None:  # no given id, retrieves all
+    if storage.get(State, s_id) is None:  # id given not found
+        abort(404)
+    elif s_id is None:  # no given id, retrieves all
         themStates = []
         for state in storage.all(State).values():
             themStates.append(state.to_dict())
         return jsonify(themStates)
-    elif storage.get(State, s_id) is None:  # id given not found
-        abort(404)
     else:  # if id is not none then it can get it
         return jsonify(storage.get(State, s_id).to_dict())
