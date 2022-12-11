c3c143439c5c4112836344b37cefdd6875c60b5a
Denisse <landau@192.168.1.8>
2022-12-11 18:22:06 -0300

    see if get works with the checkers standard

---
+++ api/v1/views/states.py
@@ -0,0 +1,22 @@
+#!/bin/usr/python3
+"""states views module"""
+from flask import Flask, request, abort, jsonify
+from models import storage
+from api.v1.views import app_views
+from models.state import State
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<s_id>', methods=['GET'], strict_slashes=False)
+def get_state(s_id=None)
+    """if no state id is given, retrieves all"""
+    if s_id is None:  #  no given id, retrieves all
+        themStates = []
+        for state in storage.all(State).values():
+            themStates.append(state.to_dict())
+        return jsonify(themStates)
+    elif storage.get(State, s_id) is None:  #  id given not found
+        abort(404)
+    else:  #  if id is not none then it can get it
+        return jsonify(storage.get(State, s_id).to_dict())
+    
