81ea332eafdbc0fc26cf33954c92e518d5b219d0
Denisse <landau@192.168.1.8>
2022-12-11 17:32:08 -0300

    getenv stuff

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,18 +7,26 @@ from models import storage
 from api.v1.views import app_views
 
 
-host = os.getenv('HBNB_API_HOST')
-port = os.getenv('HBNB_API_PORT')
+try:
+    host = os.getenv('HBNB_API_HOST')
+except:
+    host = '0.0.0.0'
+try:
+    port = os.getenv('HBNB_API_PORT')
+except:
+    port = 5000
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-if __name__ == "__main__":
-    app.run(host=getenv(host or '0.0.0.0'), port=(port or 5000),
-            threaded=True, debug=True)
-
 
 @app.teardown_appcontext
 def tear_down(exception):
     storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host=host, port=port,
+            threaded=True, debug=True)
+
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """index module"""
 from api.v1.views import app_views
-import json
 
 
 @app_views.route('/status')
