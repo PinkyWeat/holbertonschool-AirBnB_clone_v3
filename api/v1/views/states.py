1cbb50d215bf71757aa8845e912a45fb91b55cf4
Denisse <landau@192.168.1.8>
2022-12-11 19:29:43 -0300

    delete

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -24,7 +24,7 @@ def get_state(s_id=None):
     # if id is not none then it can get it
         return jsonify(storage.get(State, s_id).to_dict())
 
-@app_views.route("/states/<state_id>", methods=['DELETE'], strict_slashes=False)
+@app_views.route("/states/<s_id>", methods=['DELETE'], strict_slashes=False)
 def del_state(s_id):
     """deletes state"""
     if storage.get(State, s_id) is None:
