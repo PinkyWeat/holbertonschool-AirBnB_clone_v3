53950153433590716e0f9444e770ded2d128d192
Denisse <landau@192.168.1.8>
2022-12-11 12:39:24 -0300

    left the return None in get method inside the for, thats why it didnt work, hopefully

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -80,7 +80,7 @@ class DBStorage:
         for obj in self.all(cls).values():
             if obj.id == id:
                 return obj
-            return None
+        return None
     
     def count(self, cls=None):
         """method counts the number of objects in storage"""
