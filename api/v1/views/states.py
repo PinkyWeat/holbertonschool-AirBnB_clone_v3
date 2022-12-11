c65d48ea25c05c0747e2ef79b22521f199c26b9c
Denisse <landau@192.168.1.8>
2022-12-11 19:01:26 -0300

    separeted into 2 routes

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,15 +9,17 @@ from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_all_states():
+    """gets all states"""
+    themStates = []
+    for state in storage.all(State).values():
+        themStates.append(state.to_dict())
+    return jsonify(themStates)
+
 @app_views.route('/states/<s_id>', methods=['GET'], strict_slashes=False)
 def get_state(s_id=None):
-    """if no state id is given, retrieves all"""
+    """if no state id is given four o four error, else prints the state"""
     if storage.get(State, s_id) is None:  # id given not found
         abort(404)
-    elif s_id is None:  # no given id, retrieves all
-        themStates = []
-        for state in storage.all(State).values():
-            themStates.append(state.to_dict())
-        return jsonify(themStates)
     else:  # if id is not none then it can get it
         return jsonify(storage.get(State, s_id).to_dict())
