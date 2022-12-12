9f3fae718c8534b3a86eefb1b4cf879bf75eb017
Denisse <landau@192.168.1.8>
2022-12-11 21:52:09 -0300

    del method

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -2,12 +2,11 @@
 """amenities views module"""
 
 
-from flask import Flask, request, abort, jsonify
+from flask import request, abort, jsonify
 from models import storage
 from api.v1.views import app_views
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+
 
 @app_views.route('amenities/', methods=['GET'], strict_slashes=False)
 def get_all_amenities():
@@ -18,7 +17,9 @@ def get_all_amenities():
         themAmenities.append(amenity.to_dict())
     return jsonify(themAmenities)
 
-@app_views.route('amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+
+@app_views.route('amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_by_id(amenity_id):
     """gets amenity by id"""
     amenity = storage.get(Amenity, amenity_id)
@@ -26,3 +27,14 @@ def get_by_id(amenity_id):
         abort(404)
     return jsonify(amenity.to_dict())
 
+
+@app_views.route("/amenities/<amenity_id>", strict_slashes=False,
+                 methods=['DELETE'])
+def del_amenity(amenity_id):
+    """deletes an amenity"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is none:
+        abort(404)
+    amenity.delete()
+    storage.save()
+    return ({}), 200
