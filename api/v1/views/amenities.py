76c695780a01abc79e3130b968ef41794efc435d
Denisse <landau@192.168.1.8>
2022-12-11 22:12:40 -0300

    pep8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -55,7 +55,7 @@ def post_amenity():
 
 
 @app_views.route("/amenities/<amenity_id>", methods=['PUT'],
-                strict_slashes=False)
+                 strict_slashes=False)
 def put_amenity(amenity_id):
     """puts updated amenity instance"""
     amenity = storage.get(Amenity, amenity_id)
