04b2bcee036da0ae53599de87f6658e97b2ccf55
Denisse <landau@192.168.1.8>
2022-12-11 22:57:32 -0300

    reviews

---
+++ api/v1/views/places_reviews.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""reviews views module"""
+
+
+from api.v1.views import app_views
+from flask import abort, request, jsonify
+from models import storage
+from models.place import Place
+from models.user import User
+from models.review import Review
+
+
+@app_views.route("/places/<place_id>/reviews",
+                 strict_slashes=False, methods=['GET'])
+def get_reviews(place_id):
+    review = storage.get(Place, place_id)
+    themReviews = []
+    if review is None:
+        abort(404)
+    for rev in review.reviews:
+        themReviews.append(rev.to_dict())
+    return jsonify(themReviews)
+
+@app_views.route("/reviews/<review_id>", strict_slashes=False, methods=['GET'])
+def get_reviews(review_id):
+    """get places by id"""
+    review = storage.get(Review, review_id)
+    if review_id is None:
+        abort(404)
+    return jsonify(review.to_dict())
