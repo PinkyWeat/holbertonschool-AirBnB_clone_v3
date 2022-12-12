7a5b3e305b22836a66109b47f1544be6bc214d57
Denisse <landau@192.168.1.8>
2022-12-11 23:09:53 -0300

    whats wrong

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""reviews views module"""
-
-
+"""review views module"""
 from api.v1.views import app_views
-from flask import abort, request, jsonify
-from models import storage
+from flask import jsonify, request, abort
 from models.place import Place
-from models.user import User
 from models.review import Review
+from models.user import User
+from models import storage
 
 
-@app_views.route("/places/<place_id>/reviews",
-                 methods=['GET'], strict_slashes=False,)
+@app_views.route("/places/<place_id>/reviews", methods=['GET'], strict_slashes=False,)
 def get_reviews(place_id):
+    """gets all reviews"""
     place = storage.get(Place, place_id)
     themReviews = []
     if place is None:
@@ -22,7 +20,7 @@ def get_reviews(place_id):
     return jsonify(themReviews)
 
 
-@app_views.route("/reviews/<review_id>",  methods=['GET'], strict_slashes=False)
+@app_views.route("/reviews/<review_id>", methods=['GET'], strict_slashes=False)
 def get_reviews(review_id):
     """get places by id"""
     review = storage.get(Review, review_id)
@@ -31,8 +29,7 @@ def get_reviews(review_id):
     return jsonify(review.to_dict())
 
 
-@app_views.route("/reviews/<review_id>",
-                 strict_slashes=False, methods=['DELETE'])
+@app_views.route("/reviews/<review_id>", strict_slashes=False, methods=['DELETE'])
 def del_review(review_id):
     """deletes reviews by id"""
     review = storage.get(Review, review_id)
