aa21683a49c0c10a5ad9485fffdea27ad41277f7
Denisse <landau@192.168.1.8>
2022-12-11 21:42:43 -0300

    get method

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""amenities views module"""
+
+
+from flask import Flask, request, abort, jsonify
+from models import storage
+from api.v1.views import app_views
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+@app_views.route('amenities/', methods=['GET'], strict_slashes=False)
+def get_all_amenities():
+    """gets all amenities"""
+    themAmenities = []
+    for amenity in storage.all(Amenity).values():
+        themAmenities.append(Amenity.to_dict())
+    return jsonify(themAmenities)
+
+@app_views.route('amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+def get_by_id(amenity_id):
+    """gets amenity by id"""
+    if storage.get(Amenity, amenity_id) is None:
+        abort(404, description="Not found")
+    return jsonify(storage.get(Amenity, amenity_id).to_dict())
+
